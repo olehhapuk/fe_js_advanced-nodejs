507bc53ca41c273dcf79a82171b434b03b2f4277
olehhapuk <oleggapuk20@gmail.com>
2022-04-19 19:23:54 +0300

    Simple express server

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-const http = require('http');
-const fs = require('fs').promises;
+const express = require('express');
 const path = require('path');
 
-const PORT = 5000;
+const app = express();
 
-const contentType = {
-  '.html': 'text/html',
-  '.css': 'text/css',
-  '.js': 'text/javascript',
-};
+app.use(express.static(path.join(process.cwd(), 'public')));
 
-/**
- * req: request(запит)
- * res: response(відповідь)
- */
-const server = http.createServer(async (req, res) => {
-  let fileName = req.url.substring(1);
-  if (req.url === '/') {
-    fileName = 'index.html';
-  }
-
-  const ext = path.extname(fileName);
-  res.setHeader('Content-Type', contentType[ext] || 'text/plain');
-
-  const filePath = path.join(process.cwd(), fileName);
-  const data = await fs.readFile(filePath);
-
-  res.end(data);
+app.get('/', (req, res) => {
+  res.send("I'm working!");
 });
 
-// http://localhost:5000
-server.listen(PORT);
+app.get('/123', (req, res) => {
+  res.send("I'm 123");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
