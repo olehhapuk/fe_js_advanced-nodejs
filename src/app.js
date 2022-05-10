f2760bba09905313831fbfdcc3f75f13468bdb19
olehhapuk <oleggapuk20@gmail.com>
2022-05-10 18:54:27 +0300

    Added info, not found, file storage

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const { nanoid } = require('nanoid');
+const fs = require('fs').promises;
 
 const app = express();
 
 const viewsPath = path.join(process.cwd(), 'views');
+const contactsPath = path.join(process.cwd(), 'db/contacts.json');
+
 app.set('views', viewsPath);
 app.set('view engine', 'ejs');
 
@@ -15,11 +18,41 @@ app.use(express.urlencoded({ extended: false }));
  *   id, name, phone, address
  * }
  */
-let contacts = [];
 
-app.get('/', (req, res) => {
+async function findAll() {
+  const allContacts = JSON.parse(await fs.readFile(contactsPath));
+  return allContacts;
+}
+
+async function create({ name, phone, address }) {
+  const newContact = {
+    id: nanoid(),
+    name,
+    phone,
+    address,
+  };
+
+  const allContacts = await findAll();
+  allContacts.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+}
+
+async function deleteContact(contactId) {
+  let allContacts = await findAll();
+  allContacts = allContacts.filter((item) => item.id !== contactId);
+
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+}
+
+async function findById(contactId) {
+  const allContacts = await findAll();
+  return allContacts.find((item) => item.id === contactId);
+}
+
+app.get('/', async (req, res) => {
   res.render('index', {
-    contacts,
+    contacts: await findAll(),
   });
 });
 
@@ -27,22 +60,33 @@ app.get('/create', (req, res) => {
   res.render('create');
 });
 
-app.post('/create', (req, res) => {
-  contacts.push({
-    ...req.body,
-    id: nanoid(),
-  });
+app.post('/create', async (req, res) => {
+  await create(req.body);
+  res.redirect('/');
+});
+
+app.get('/delete/:contactId', async (req, res) => {
+  const { contactId } = req.params;
+  await deleteContact(contactId);
 
   res.redirect('/');
 });
 
-app.get('/delete/:contactId', (req, res) => {
-  const { contactId } = req.params;
+app.get('/contact/:contactId', async (req, res) => {
+  const contactId = req.params.contactId;
+  const contact = await findById(contactId);
+  if (!contact) {
+    res.redirect('/404');
+    return;
+  }
 
-  const filteredContacts = contacts.filter((item) => item.id !== contactId);
-  contacts = filteredContacts;
+  res.render('info', {
+    contact,
+  });
+});
 
-  res.redirect('/');
+app.get('/404', (req, res) => {
+  res.render('not-found');
 });
 
 module.exports = app;
