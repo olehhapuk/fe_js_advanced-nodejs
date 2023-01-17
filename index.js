ef020be3e230a2a372f0f2f3f0c3cd3a609a5b16
olehhapuk <oleggapuk20@gmail.com>
2023-01-17 19:31:58 +0200

    added mongo connection url example

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res) => res.send('Hello, World!'));
 
+const templateMongoUrl = 'mongodb://<username>:<password>@<service-name>:27017';
+const exampleMongoUrl = 'mongodb://root:root@database:27017';
+
 app.listen(5000, () => {
   console.log('Server started');
 });
